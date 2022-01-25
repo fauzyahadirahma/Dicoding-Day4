241e64834d3271e52087b837355300cc9f87fa13
Fauzyah Hadirahma <97279376+fauzyahadirahma@users.noreply.github.com>
2022-01-26 01:58:12 +0700

    Inisialisasi dan Assigment Nilai, 12 Januari 2022

---
+++ main.c
@@ -0,0 +1,15 @@
+/* Penulis   : Fauzyah Hadirahma, email fauzyah.hadirahma@gmail.com */
+/* Deskripsi : Inisialisasi dan Assigment Nilai Bilangan Rill */
+
+int main () {
+    /* KAMUS */
+    float f = 0.555;
+    float x = 1.5E3;
+    
+    /* ALGORITMA */
+    printf ("Ini nilai f : %f \n", f);
+    printf ("Ini nilai f : %5.2f \n", f);
+    printf ("Ini nilai x : %10.2f \n", f);
+    
+    return 0;
+}
